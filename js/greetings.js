6abb365fdd2a24cd56c30a5c10cff3ba2c9f0385
GodDorae <87344789+GodDorae@users.noreply.github.com>
2021-09-01 23:01:45 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
+
+const clock_content = document.querySelector("#clock");
+const todo_content = document.querySelector("#todo-content");
+const quote_content = document.querySelector("#quote");
+
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,6 +13,9 @@ const USERNAME_KEY = "username";
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  clock_content.classList.remove(HIDDEN_CLASSNAME);
+  todo_content.classList.remove(HIDDEN_CLASSNAME);
+  quote_content.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLoginSubmit(event) {
